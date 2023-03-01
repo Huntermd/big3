78fb8265669c2704ec5cd9cba1ee2a6673c3d491
Hunter Davis <huntermd111@gmail.com>
2023-03-01 14:34:07 -0800

    created the health screen

---
+++ screens/HealthScreen.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View , Pressable} from 'react-native'
+import React from 'react'
+import TimeSteps from '../components/TimeSteps'
+
+export default function HealthScreen({navigation}) {
+  return (
+    <View>
+      <Text style={styles.HeadlineText}>Health and Fitness</Text>
+      <TimeSteps/>
+      <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
+        <Text style={{color: 'grey'}}>Back</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+  backButton: {
+    width: '35%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 32,
+    borderWidth: 2,
+    borderColor: 'grey',
+    borderRadius: 12,
+    
+    
+  },
+  HeadlineText:{
+    color: 'grey',
+    fontSize: 40,
+    margin: 30
+  },
+  
+})
