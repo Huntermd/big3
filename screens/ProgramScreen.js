eeae3d46b2ea8e93baf3b969c082742c674b66c4
Hunter Davis <huntermd111@gmail.com>
2023-03-10 10:13:39 -0800

    Created a button that would return Accesorys.js

--- screens/ProgramScreen.js
+++ screens/ProgramScreen.js
@@ -1,15 +1,38 @@
-import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native'
+import { StyleSheet, Text, View, Pressable, TextInput, Button } from 'react-native'
 import React from 'react'
 import MainLift from '../components/MainLift'
 import Accesorys from '../components/Accesorys'
+import { useState } from 'react'
+
 export default function ProgramScreen({navigation}) {
+
+  const [component,setComponent] = useState([]);
+
+  const handlePress = () => {
+
+    setComponent(component => [...component, '1']);
+  }
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.myText}>Week 1 Day 1</Text>
       <MainLift lift = 'squat' sets = '1' reps = '4' weight = '270'  downSets = '4' downReps = '4' downWeight = '250'/>
-      <Accesorys sets = '3'  reps= '10' weight = ''/>
-      <Accesorys sets = '4'  reps= '8' weight = ''/>
-      <Accesorys sets = '5'  reps= '15' weight = ''/>
+      <Accesorys  />
+      <Accesorys   />
+      <Accesorys   />
+
+    {  component.map(() => {
+        return <Accesorys />;
+      })}
+
+
+      <Pressable  onPress={handlePress} >
+      
+        <View>
+          <Text> Press me </Text>
+        </View>
+      </Pressable>
       
       <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
         <Text style={{color: 'grey'}}>Back</Text>
