55763f0226ef40eb6f8b85154f519622629301c9
Hunter Davis <huntermd111@gmail.com>
2023-03-10 10:14:12 -0800

    created sets object

--- helpers/data.js
+++ helpers/data.js
@@ -22,6 +22,22 @@ const data = {
         ],
     },
 
+    Sets:[
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        'Density',
+        'Cluster set',
+        'Drop Set',
+    ],
+
     workouts: [],
 
     events: []
