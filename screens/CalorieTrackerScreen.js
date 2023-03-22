b8c9dacf7bc18c6dcadc4eb12f84c9ea0273b944
Hunter Davis <huntermd111@gmail.com>
2023-03-22 12:21:17 -0700

    Added an Api call

--- screens/CalorieTrackerScreen.js
+++ screens/CalorieTrackerScreen.js
@@ -1,53 +1,108 @@
-import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native'
+import { Pressable, StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
+import { useState, useEffect, useContext, createContext  } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { useState, useEffect } from 'react';
+import NavCal from '../components/NavCal';
+import FoodData from '../components/FoodData';
+import axios from 'axios';
+
+let reqInstance = axios.create({
+headers:{
+  'x-app-id': 'bcdfedf4',
+  'x-app-key': 'c0ba995c96bbbe9be1e53f50814145f0',
+  'x-remote-user-id': '0',
+  'content-type': 'application/json',
+  'accept': 'application/json'
+}
+
+});
+
+
+
+export const BreakfastContext = createContext()
 
 export default function CalorieTrackerScreen() {
+  const [NutData, setNutData] = useState();
 
-   const [Breakfast, setBreakfast] = useState(0)
-   const [Snack1, setSnack1] = useState(0)
-   const [Lunch, setLunch] = useState(0)
-   const [Snack2, setSnack2] = useState(0)
-   const [Dinner, setDinner] = useState(0)
+  useEffect(() => {
+    reqInstance.get('https://trackapi.nutritionix.com/v2/search/instant?query=apple').then(  (response) => 
+ { console.log(response);
+  setNutData(response.Object.data);
+  
+})
+.catch((error) => {
+  console.log(error);
+})
+ 
 
-    const navigation = useNavigation();
+}, []);
+  
+  const [Nutrient,setNutrient] = useState([]);
+   const navigation = useNavigation();
+   const [Breakfast,setBreakfast] = useState(100);
+const [Lunch,setLunch] = useState(100);
+const [Dinner,setDinner] = useState(199);
+
+
   return (
-    <View>
-      <Text>CalorieTrackerScreen</Text>
-      <TextInput style={styles.CalInput} 
-      placeholder='2000'
-      onChangeText={(val) => setBreakfast(val)}
-      />
+    <ScrollView>
+     
 
-      <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
+
+
+
+
+     <View style={styles.Container}>
+    <View style={styles.NavBar}>
+    <NavCal name = 'Breakfast' cal = {Breakfast}/>
+    <NavCal name = 'Breakfast' cal = {Lunch}/>
+    <NavCal name = 'Breakfast' cal = {Dinner}/>
+    </View>
+    <Text>{NutData.branded[0].food_name}</Text>
+    {  Nutrient.map((data) => {
+        return <FoodData/>;
+      })}
+
+
+
+    <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
         <Text style={{color: 'grey'}}>Back</Text>
       </Pressable>
-    </View>
+     </View>
+
+    </ScrollView>
   )
 }
 
-
-
 const styles = StyleSheet.create({
-    backButton: {
-        width: '35%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 32,
-        borderWidth: 2,
-        borderColor: 'grey',
-        borderRadius: 12,
-        
-        
-      },
-      CalInput:{
-        borderColor: 'black',
-        borderWidth: 1,
-        width: 60,
-        height: 35,
-        
-        
-      },
+  Container:{
+   marginTop: 70,
+   width: '100%',
+   height: 'auto',
+   borderColor: 'black',
+   borderWidth: 2,
+   minHeight: '90%',
+   alignItems: 'center'
 
+   
+  },
+  backButton: {
+    width: '35%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 32,
+    borderWidth: 2,
+    borderColor: 'grey',
+    borderRadius: 12,
+    
+    
+  },
+  NavBar:{
+    flexDirection: 'row',
+    width: '95%',
+    borderColor: 'black',
+    borderWidth: 2,
+    
+  }
 })
+
