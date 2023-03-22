084905ebce075411758eb06d9f0f8331d4aaad4b
Hunter Davis <huntermd111@gmail.com>
2023-03-22 12:25:04 -0700

    change text button

--- screens/DeadLiftScreen.js
+++ screens/DeadLiftScreen.js
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable, ImageBackground } from 'react-native';
 import bg from '../assets/belt2.jpg';
-
+import { useState } from 'react';
+import data from '../helpers/data';
 const DeadLiftScreen = ({navigation}) => {
+
+ 
+
+
+const [Summary, setText] = useState(data.Summary.Squat.BackSquat);
+
+const handlePress = () => {
+      
+  data.Summary.Squat.FrontSquat
+setText(data.Summary.Squat.FrontSquat);
+}
+
+
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} resizeMode="cover" style={styles.bgImage} >
       <Text style={{color: 'white'}}>DeadLiftScreen</Text>
 
+    <Text style={styles.Header}>{Summary.Header} </Text>
+
+<Pressable style={styles.backButton} onPress={handlePress}>
+  <Text style={{color: 'white'}}>Press me </Text>
+</Pressable>
       <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
         <Text style={{color: 'white'}}>Back</Text>
       </Pressable>
@@ -38,5 +57,9 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'white',
     borderRadius: 12
+  },
+  Header:{
+    color: 'white',
+    fontSize: 20
   }
 });
